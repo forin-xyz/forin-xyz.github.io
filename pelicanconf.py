6f0cd450b5d25934f529da8ec580aa256e0225ae
forin.xyz <0000@whu.edu.cn>
2016-02-18 18:09:07 +0800

    机器学习

--- pelicanconf.py
+++ pelicanconf.py
@@ -42,7 +42,7 @@ DEFAULT_DATE_FORMAT = '%Y年%m月%d日 星期%a'
 # 静态文件
 STATIC_PATHS = ['blog', 'downloads', 'images', 'pdfs']
 # 文章路径
-ARTICLE_PATHS = ['计算机科学', '数学', 'computer']
+ARTICLE_PATHS = ['计算机科学', '数学', '机器学习', '随笔']
 
 DUOSHUO_SITENAME = "耕有田，读有书"
 
