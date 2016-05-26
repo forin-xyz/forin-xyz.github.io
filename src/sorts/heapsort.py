426955048eb63c3cb9affc249c18f015a10cb61c
chengrui <chengrui@mirrtalk.com>
2016-05-26 17:21:41 +0800

    sort information

--- src/sorts/heapsort.py
+++ src/sorts/heapsort.py
@@ -66,3 +66,55 @@ def _siftup(heap, pos):
     # to its final resting place (by sifting its parents down).
 
     heap[pos] = newitem
+
+def heappush(heap, item):
+
+    """Push item onto heap, maintaining the heap invariant."""
+
+    heap.append(item)
+
+    _siftdown(heap, 0, len(heap)-1)
+
+
+def heappop(heap):
+
+    """Pop the smallest item off the heap, maintaining the heap invariant."""
+
+    lastelt = heap.pop()    # raises appropriate IndexError if heap is empty
+
+    if heap:
+
+        returnitem = heap[0]
+
+        heap[0] = lastelt
+
+        _siftup(heap, 0)
+
+        return returnitem
+
+    return lastelt
+
+def heapify(x):
+
+    """Transform list into a heap, in-place, in O(len(x)) time."""
+
+    n = len(x)
+
+    # Transform bottom-up.  The largest index there's any point to looking at
+
+    # is the largest with a child index in-range, so must have 2*i + 1 < n,
+
+    # or i < (n-1)/2.  If n is even = 2*j, this is (2*j-1)/2 = j-1/2 so
+
+    # j-1 is the largest, which is n//2 - 1.  If n is odd = 2*j+1, this is
+
+    # (2*j+1-1)/2 = j so j-1 is the largest, and that's again n//2-1.
+
+    for i in reversed(range(n//2)):
+
+        _siftup(x, i)
+
+
+def heapsort(lst):
+    heapify(lst)
+    return [heappop(lst) for i in range(len(h))]
